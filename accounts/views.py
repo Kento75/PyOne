4a0acb4be3d95d6aba0db6ad2502e343090eeea7
Kento75 <kentonet75@gmail.com>
2018-03-29 12:14:58 +0900

    カスタムログイン画面の実装

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
+from django.views.generic import View
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+@python_2_unicode_compatible
+class Index(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        return render(request, 'accounts/index.html')
