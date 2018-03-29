eb917b4dbae22b7b6e54029f37a7409df7919a5b
Kento75 <kentonet75@gmail.com>
2018-03-29 13:50:04 +0900

    bootstrapとbase.htmlの追加

--- PyOne/settings.py
+++ PyOne/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 LOGIN_ERROR_URL = '/accounts/login'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/accounts/index/'
