6a32d7549977fa613111262ca6a8633307f07493
Kento75 <kentonet75@gmail.com>
2018-03-31 12:49:12 +0900

    mapファイルの追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login, {'template_name': 'accounts/login.html'}, name='login'),
     path('logout/', logout, {'template_name': 'accounts/login.html'}, name='logout'),
     path('index/', Index.as_view(), name='account_index'),
+#    path('change_password/', change_password, name='change_password'),
 ]
