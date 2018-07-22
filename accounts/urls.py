0944f7768e63486a897b2ae7de02d70da10c4afe
Kento75 <kento2github@gmail.com>
2018-07-22 16:08:02 +0900

    URL変更

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from django.contrib.auth.views import login, logout
 from django.contrib.auth.decorators import login_required
 from .views import Index, PasswordChange, PasswordChangeDone
+from django.views.generic.base import RedirectView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/accounts/login/')),                                     # ログイン画面へ遷移
     path('login/', login, {'template_name': 'accounts/login.html'}, name='login'),     # ログイン画面へ遷移
     path('logout/', logout, {'template_name': 'accounts/login.html'}, name='logout'),  # ログアウト→ログイン画面へ遷移
     path('index/', Index.as_view(), name='account_index'),                             # ログイン→ホーム画面へ遷移
